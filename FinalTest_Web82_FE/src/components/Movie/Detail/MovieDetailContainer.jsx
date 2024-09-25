46bc8bb60e166caa8b7062be66579abd656cd952
phucts123456 <hooquangphuc.t4@gmail.com>
2024-09-26 03:36:30 +0700

    remove unnecessary

--- FinalTest_Web82_FE/src/components/Movie/Detail/MovieDetailContainer.jsx
+++ FinalTest_Web82_FE/src/components/Movie/Detail/MovieDetailContainer.jsx
@@ -6,7 +6,7 @@ const MovieDetailContainer = ({movie, isOpen, setIsOpen}) => {
                 <img src={movie.image === "" ? "./img/defaultFilm.jpg" :movie.image} className="movie_detail_left_image"/>
             </div>
             <div className="movie_detail_right">
-                <h3 className="movie_detail_right_title">{movie.name}dddddddddddddddddddddddddddddddddddddddddddddddddddd</h3>
+                <h3 className="movie_detail_right_title">{movie.name}</h3>
                 <p className="movie_detail_right_info">{movie.time} min {movie.year}</p>
                 <p className="movie_detail_right_des">{movie.introduce}</p>
                 <button className="movie_detail_right_btn"><img src="./img/play-16.png" style={{marginRight:'10px'}} className="movie_detail_close_button" />PLAY MOVIE</button>
